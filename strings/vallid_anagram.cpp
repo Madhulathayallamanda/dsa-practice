7d22e98bbc064f14d43f6cd8bfab35095ae49d26
Yallamanda Madhulatha <madhulathayallamanda@gmail.com>
2025-12-19 11:03:35 +0530

    Create vallid_anagram.cpp

---
+++ strings/vallid_anagram.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "anagram", t = "nagaram";
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    cout << (s == t);
+}
